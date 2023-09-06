ba0cea53849595e72c504b8b025e0b09783083bc
WangTao <tomwtg@gmail.com>
2023-09-06 16:40:58 +0800

    Update idccalculator.py

--- idccalculator.py
+++ idccalculator.py
@@ -110,38 +110,38 @@ with st.sidebar:
 
 def make_final_df(df):
 
-    df['S累计']=df['440w服务器月增数'].cumsum()
-    df['计算累计']=df['4000w服务器月增数'].cumsum()
-    df['推理累计']=df['1500w服务器月增数'].cumsum()
-    df['S机柜']=np.ceil(df['S累计']/10)
-    df['计算机柜']=np.ceil(df['计算累计'])
-    df['推理机柜']=np.ceil(df['推理累计']/3)
+    df['小服务器累计']=df['440w服务器月增数'].cumsum()
+    df['大服务器累计']=df['4000w服务器月增数'].cumsum()
+    df['中服务器累计']=df['1500w服务器月增数'].cumsum()
+    df['低电机柜']=np.ceil(df['小服务器累计']/10)
+    df['高电机柜']=np.ceil(df['大服务器累计'])
+    df['中电机柜']=np.ceil(df['中服务器累计']/3)
     df['百G专线']=np.ceil(df['百G专线条数'])
     #st.dataframe(df)
 
-    jigui_df=df[['月','S机柜','计算机柜','推理机柜','百G专线']]
+    jigui_df=df[['月','低电机柜','高电机柜','中电机柜','百G专线']]
     #st.dataframe(jigui_df)
 
-    A_df=jigui_df[['月','S机柜','计算机柜','推理机柜','百G专线']]
-    A_df['机柜成本']=(jigui_df['S机柜']+jigui_df['计算机柜']+jigui_df['推理机柜'])*idc_power_price_dct['A-4.4-price']
+    A_df=jigui_df[['月','低电机柜','高电机柜','中电机柜','百G专线']]
+    A_df['机柜成本']=(jigui_df['低电机柜']+jigui_df['高电机柜']+jigui_df['中电机柜'])*idc_power_price_dct['A-4.4-price']
     A_df['专线成本']=(jigui_df['百G专线'])*A_line
     #st.text('A_df')
     #st.dataframe(A_df)
 
-    B_df=jigui_df[['月','S机柜','计算机柜','推理机柜','百G专线']]
-    B_df['机柜成本']=(jigui_df['S机柜']+jigui_df['计算机柜']+jigui_df['推理机柜'])*idc_power_price_dct['B-4.4-price']
+    B_df=jigui_df[['月','低电机柜','高电机柜','中电机柜','百G专线']]
+    B_df['机柜成本']=(jigui_df['低电机柜']+jigui_df['高电机柜']+jigui_df['中电机柜'])*idc_power_price_dct['B-4.4-price']
     B_df['专线成本']=(jigui_df['百G专线'])*B_line
     #st.text('B_df')
     #st.dataframe(B_df)
 
-    C_df=jigui_df[['月','S机柜','计算机柜','推理机柜','百G专线']]
-    C_df['机柜成本']=(jigui_df['S机柜']+jigui_df['计算机柜']+jigui_df['推理机柜'])*idc_power_price_dct['C-4.4-price']
+    C_df=jigui_df[['月','低电机柜','高电机柜','中电机柜','百G专线']]
+    C_df['机柜成本']=(jigui_df['低电机柜']+jigui_df['高电机柜']+jigui_df['中电机柜'])*idc_power_price_dct['C-4.4-price']
     C_df['专线成本']=(jigui_df['百G专线'])*C_line
     #st.text('C_df')
     #st.dataframe(C_df)
 
-    D_df=jigui_df[['月','S机柜','计算机柜','推理机柜','百G专线']]
-    D_df['机柜成本']=(jigui_df['S机柜']+jigui_df['计算机柜']+jigui_df['推理机柜'])*idc_power_price_dct['D-4.4-price']
+    D_df=jigui_df[['月','低电机柜','高电机柜','中电机柜','百G专线']]
+    D_df['机柜成本']=(jigui_df['低电机柜']+jigui_df['高电机柜']+jigui_df['中电机柜'])*idc_power_price_dct['D-4.4-price']
     D_df['专线成本']=(jigui_df['百G专线'])*D_line
     #st.text('D_df')
     #st.dataframe(D_df)
@@ -191,7 +191,7 @@ with col2:
     st.line_chart(low_jigui_df[['百G专线']])
 with col3:
     st.title('机柜')
-    st.line_chart(low_jigui_df[['S机柜','计算机柜','推理机柜','百G专线']]) 
+    st.line_chart(low_jigui_df[['低电机柜','高电机柜','中电机柜','百G专线']]) 
 
 
 col1, col2, col3 = st.columns([3,1,1])
@@ -204,7 +204,7 @@ with col2:
     st.line_chart(jigui_df[['百G专线']])
 with col3:
     st.title('机柜')
-    st.line_chart(jigui_df[['S机柜','计算机柜','推理机柜','百G专线']])
+    st.line_chart(jigui_df[['低电机柜','高电机柜','中电机柜','百G专线']])
 
 col1, col2, col3 = st.columns([3,1,1])
 with col1:
@@ -216,4 +216,4 @@ with col2:
     st.line_chart(high_jigui_df[['百G专线']])
 with col3:
     st.title('机柜')
-    st.line_chart(high_jigui_df[['S机柜','计算机柜','推理机柜','百G专线']])
+    st.line_chart(high_jigui_df[['低电机柜','高电机柜','中电机柜','百G专线']])
